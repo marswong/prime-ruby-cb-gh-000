a44ca14a1d92fd72459b0e02caee723460c7adf2
Mars Wong <marswong618@gmail.com>
2018-01-26 04:34:42 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+def prime?(n)
+  if n <= 1
+    false
+  elsif n <= 3
+    true
+  elsif n % 2 == 0 || n % 3 == 0
+    false
+  else
+    i = 5
+    result = true
+    while i ** 2 < n
+      if n % i == 0 || n % (i + 2) == 0
+        result = false
+      end
+      i += 6
+    end
+    result
+  end
+end
