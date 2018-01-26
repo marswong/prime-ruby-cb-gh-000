7a8762d2b8e7c096e61a31e233b476d3f85c8b36
Mars Wong <marswong618@gmail.com>
2018-01-26 04:27:54 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -6,4 +6,13 @@ def prime?(n)
   elsif n % 2 == 0 || n % 3 == 0
     false
   end
+
+  i = 5
+  while i ** 2 < n
+    if n % i == 0 || n % (i + 2) == 0
+      false
+    end
+    i += 6
+  end
+  true
 end
